0fa32eb81e444cc284764fbbf6d7ed54dde48534
Lauren <eastridgele@gmail.com>
2013-03-27 23:29:42 -0700

    notify sends texts for high priority tasks

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -1,10 +1,13 @@
 class NotificationController < ApplicationController
   def index
+    # Membership.group(:user_email).each do |user|
+    #   Notifier.weekly_email(user.user_email).deliver
+    # end
+    # Membership.group(:user_email).each do |user|
+    #   Notifier.daily_email(user.user_email).deliver
+    # end
     Membership.group(:user_email).each do |user|
-      Notifier.weekly_email(user.user_email).deliver
-    end
-    Membership.group(:user_email).each do |user|
-      Notifier.daily_email(user.user_email).deliver
+      Notifier.urgent_notification(user.user_email)
     end
     redirect_to root_path
   end
--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -43,6 +43,40 @@ class Notifier < ActionMailer::Base
     end
   end
 
+  #priority = 3
+  def urgent_notification(user_email)
+    content = ""
+    address = user_email
+    logger.info 'email: ' + user_email
+    logger.info User.where(:email, user_email).first
+    phone = User.where(email: user_email).first.phone
+    Membership.where(user_email: user_email).each do |membership|
+      Task.where(group_id: membership.group_id).each do |task|
+        if task.priority == 2
+          content << task.title
+        end
+      end
+      Subtask.where(group_id: membership.group_id).each do |subtask|
+        if subtask.priority == 2
+          content << subtask.title
+        end
+      end
+    end
+    unless phone.nil?
+      if content.empty? == false
+        TWILIO_CLIENT.account.sms.messages.create(
+          from: TWILIO_NUMBER,
+          to: "+#{phone}",
+          body: content
+        )
+      end
+    end
+
+    # unless content.empty?
+    #   mail(to: address, subject: "Daily Tasks", body: content)
+    # end
+  end
+
 end
 
 
